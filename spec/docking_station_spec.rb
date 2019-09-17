7023e314380f50b0752510581624a0ebe9c28dfd
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-17 16:44:52 +0100

    dock_bike method in DockingStation class accepts instances of Bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,9 +7,17 @@ describe DockingStation do
   describe '#release_bike' do
      it { expect(docking_station).to respond_to(:release_bike) }
 
-     # returns instance of bikes
      it { expect(docking_station.release_bike.class).to eq Bike}
+  end
 
+  describe '#dock_bike' do
+     docking_station.dock_bike(Bike.new)
+     it { expect(docking_station).to respond_to(:dock_bike) }
+     it { expect(docking_station.bike.class).to eq Bike}
+  end
+
+  describe '#bike' do
+     it { expect(docking_station).to respond_to(:bike) }
   end
 
 end
