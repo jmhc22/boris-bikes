27fcad416bae6d0c674d7a53269bb951911bd868
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-20 15:02:28 +0100

    Replaces bike objects with dummies in docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,7 +14,8 @@ describe DockingStation do
     # returns an instance of Bike
     # it { expect(docked_station.release_bike).to be_a Bike }
     it "occupied docking station will release working bike" do
-      docking_station.dock_bike(Bike.new)
+      bike = double(:bike, working?: true)
+      docking_station.dock_bike(bike)
       expect(docking_station.release_bike).to respond_to(:working?)
     end
     it "raises an error if no bikes are available" do
@@ -22,21 +23,33 @@ describe DockingStation do
     end
 
     it "docking station will not release a broken bikes" do
-      broken_bike = Bike.new
-      broken_bike.report_broken
+      broken_bike = double(:bike, working?: false)
       docking_station.dock_bike(broken_bike)
       expect { docking_station.release_bike }.to raise_error("NoWorkingBikes")
     end
 
     it "if at least one docked bike is working, this bike will be released" do
-      broken_bike = Bike.new
-      broken_bike.report_broken
-      working_bike = Bike.new
+      broken_bike = double(:bike, working?: false)
+      working_bike = double(:bike, working?: true)
       docking_station.dock_bike(broken_bike)
       docking_station.dock_bike(working_bike)
       expect(docking_station.release_bike).to eq working_bike
     end
 
+    # it 'releases working bikes' do
+    #   bike = double(:bike)
+    #   docking_station.dock_bike(bike)
+    #   allow(bike).to receive(:working?) { true } #.and_return
+    #   bike = docking_station.release_bike
+    #   expect(bike).to be_working
+    # end
+
+    it 'releases working bikes' do
+      bike = double(:bike, working?: true)
+      docking_station.dock_bike(bike)
+      bike = docking_station.release_bike
+      expect(bike).to be_working
+    end
 
 
  end
@@ -46,16 +59,17 @@ describe DockingStation do
     it { expect(docking_station).to respond_to(:dock_bike) }
     # should assign a bike instance to the @bike variable of a DockingStation instance
     it "raises an error if 20 bikes are already docked" do
-      described_class::DEFAULT_CAPACITY.times { docking_station.dock_bike(Bike.new) }
-      expect { docking_station.dock_bike(Bike.new)}.to raise_error("FullyOccupiedError")
+      bike = double(:bike)
+      described_class::DEFAULT_CAPACITY.times { docking_station.dock_bike(bike) }
+      expect { docking_station.dock_bike(bike)}.to raise_error("FullyOccupiedError")
     end
     it "dock_bike stores a bike object as an instance variable of docking_station" do
-      docking_station.dock_bike(Bike.new)
-      expect(docking_station.bikes).to include Bike
+      bike = double(:bike)
+      docking_station.dock_bike(bike)
+      expect(docking_station.bikes).to include bike
     end
     it "dock_bike docks bikes whether they are broken or not" do
-      broken_bike = Bike.new
-      broken_bike.report_broken
+      broken_bike = double(:bike, working?: false)
       docking_station.dock_bike(broken_bike)
       expect(docking_station.bikes).to include broken_bike
     end
